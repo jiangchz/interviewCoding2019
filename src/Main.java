fb3f75a425ba59a75aa84d590cf1ad2677dd0f1d
jiangchen <zjiangch@gmail.com>
2019-02-03 15:29:16 -0800

    initial commit with combinationSum1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Go fuck yourself Amholes!");
+        System.out.println("Fight on! Trojan");
+
+    }
+}
