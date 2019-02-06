de85df7ba7f0cab102ea10efc0ab00cb88b009c8
jiangchen <zjiangch@gmail.com>
2019-02-05 22:51:52 -0800

    Add merge sort

--- src/sortingAlgorithms/MergeSort.java
+++ src/sortingAlgorithms/MergeSort.java
@@ -1,32 +1,67 @@
 package sortingAlgorithms;
 
-public class SelectionSort {
-    public int[] sortArray(int[] array) {
-        if (array == null || array.length <= 1) {
+import java.util.Arrays;
+
+public class MergeSort {
+    public static void main(String args[]) {
+        int[] array = new int[] {6,5,4,3,2,1};
+        mergeSort(array);
+    }
+
+    public static int[] mergeSort(int[] array) {
+        if (array == null || array.length == 0) {
             return array;
         }
 
-        for (int outerIndex = 0; outerIndex < array.length - 1; outerIndex++) {
-            int localMin = outerIndex;
-            for (int innerIndex = outerIndex + 1; innerIndex < array.length; innerIndex++) {
-                localMin = array[innerIndex] < array[localMin] ? innerIndex : localMin;
-            }
-            swap(array, localMin, outerIndex);
-        }
+        int left = 0;
+        int right = array.length - 1;
+        sort(array, left, right);
         return array;
-
     }
-    private static void swap(int[] array, int index1, int index2) {
-        int tmp = array[index1];
-        array[index1] = array[index2];
-        array[index2] = tmp;
+    private static void sort(int arr[], int left, int right)
+    {
+        if (left >= right) {
+            return;
+        }
+        int middle = left + (right - left) / 2;
+
+        sort(arr, left, middle);
+        sort(arr , middle + 1, right);
+
+        merge(arr, left, middle, right);
+    }
+
+    private static void merge(int array[], int left, int middle, int right) {
+        int leftSize = middle - left + 1;
+        int rightSize = right - middle;
+        int leftIndex = 0;
+        int rightIndex = 0;
+        int[] leftArray = Arrays.copyOfRange(array, left, middle + 1);
+        int[] rightArray = Arrays.copyOfRange(array, middle + 1, right + 1);
+
+
+        while (leftIndex < leftSize && rightIndex < rightSize) {
+            if (leftArray[leftIndex] > rightArray[rightIndex]) {
+                array[left++] = rightArray[rightIndex++];
+            } else {
+                array[left++] = leftArray[leftIndex++];
+            }
+        }
+
+        while (leftIndex < leftSize) {
+            array[left++] = leftArray[leftIndex++];
+        }
+
+        while (rightIndex < rightSize) {
+            array[left++] = rightArray[rightIndex++];
+        }
     }
 
 }
 
 /*
 Given an array of integers, sort the elements in the array in ascending order.
-The selection sort algorithm should be used to solve this problem.
+The merge sort algorithm should be used to solve this problem.
 
 Examples
 
@@ -38,5 +73,7 @@ Corner Cases
 
 What if the given array is null? In this case, we do not need to do anything.
 What if the given array is of length zero? In this case, we do not need to do anything.
+
+坑： Array.copyOfRange end index is exclusive!
 
 */
