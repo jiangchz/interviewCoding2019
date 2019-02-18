e8b876a7b5a7367862bec55d8fbdeb9f6e292ca1
jiangchen <zjiangch@gmail.com>
2019-02-17 23:36:41 -0800

    Add MinHeap implementation

---
+++ src/heap/TreeNode.java
@@ -0,0 +1,10 @@
+package heap;
+
+public class TreeNode {
+    public int value;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int value) {
+        this.value = value;
+    }
+}
