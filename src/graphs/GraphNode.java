80e17e12bce95ad6e18a7a96a422da539d49e38a
jiangchen <zjiangch@gmail.com>
2019-02-19 00:29:42 -0800

    add Bipartite, IsComplete and KthSmallestInMatrix

--- src/graphs/GraphNode.java
+++ src/graphs/GraphNode.java
@@ -1,8 +1,21 @@
 package graphs;
 
+import java.util.ArrayList;
+
 public class GraphNode {
     public int value;
+    public char color;
+    public ArrayList<GraphNode> neighbors;
+    //todo update with builder pattern
     public GraphNode(int value) {
+        this(value, 'c', null);
+    }
+    public GraphNode(int value, ArrayList<GraphNode> neighbors) {
+       this(value, 'c', neighbors);
+    }
+    public GraphNode(int value, char color, ArrayList<GraphNode> neighbors) {
         this.value = value;
+        this.color = color;
+        this.neighbors = neighbors;
     }
 }
