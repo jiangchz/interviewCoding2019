5990a600fd87d95f46eb18c910d30f9af2c06536
jiangchen <zjiangch@gmail.com>
2019-02-26 22:46:06 -0800

    add dp1

---
+++ src/dynamicPrograming/DictionaryGame.java
@@ -0,0 +1,38 @@
+package dynamicPrograming;
+
+import java.util.HashSet;
+
+public class DictionaryGame {
+    public boolean isInDictionary(HashSet<String> dictionary, String target) {
+        int size = target.length();
+        boolean[] inDictionary = new boolean[size + 1];
+
+        inDictionary[0] = true;
+        for (int i = 1; i <= target.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if(inDictionary[j] && dictionary.contains(target.substring(j, i))) { //前j个数包含在字典里，
+                    // 第j + 1个数到 i包含在字典里 注意 这里是第J+1个数
+                    inDictionary[i] = true;
+                }
+            }
+        }
+        return inDictionary[size];
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
+
+/*
+Given a word, can it composed by concatenating words from a given dictionary?
+dp[i]: 前i个字母组成的string是否包含在字典里
+
+ */
---
+++ src/dynamicPrograming/LongestAscendingSubArray.java
@@ -0,0 +1,43 @@
+package dynamicPrograming;
+
+public class LongestAscendingSubArray {
+    public int longest(int[] array) {
+        if (array == null || array.length == 0) {
+            return 0;
+        }
+        int gobalMax = 1;
+        int previous = 1;
+        for (int index = 1; index < array.length; index++) {
+            if (array[index] > array[index - 1]) {
+                previous++;
+            } else {
+                previous = 1;
+            }
+            gobalMax = Math.max(previous, gobalMax);
+        }
+        return gobalMax;
+    }
+}
+/*
+Given an unsorted array, find the length of the longest subarray in which the numbers are in ascending order.
+
+Assumptions
+
+The given array is not null
+Examples
+
+{7, 2, 3, 1, 5, 8, 9, 6}, longest ascending subarray is {1, 5, 8, 9}, length is 4.
+
+{1, 2, 3, 3, 4, 4, 5}, longest ascending subarray is {1, 2, 3}, length is 3.
+
+
+
+
+M[i]: the longest ascending order of the first i elements
+baseCase: M[0] = 1;
+induction rule:
+               case1 : array[i] > array[i - 1]
+                     M[i] = M[i - 1] + 1
+               case2:
+                     M[i] = 1
+ */
