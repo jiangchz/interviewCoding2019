e4e83e72354ab3de952d7c66432da3957c88fc21
jiangchen <zjiangch@gmail.com>
2019-03-26 00:31:52 -0700

    add some code

--- src/graphsAndSearch/bfs/CloneGraph.java
+++ src/graphsAndSearch/bfs/CloneGraph.java
@@ -4,34 +4,68 @@ import java.util.*;
 
 public class CloneGraph {
     public List<GraphNode> copy(List<GraphNode> graph) {
+        Set<GraphNode> expanded = new HashSet<>();
+        List<GraphNode> copiedGraph = new ArrayList<>();
+        Queue<GraphNode> toExpandNodes = new LinkedList<>();
         Map<GraphNode, GraphNode> nodeMapping = new HashMap<>();
-        Set<GraphNode> explored = new HashSet<>();
-        Queue<GraphNode> toExplore = new LinkedList<>();
-        List<GraphNode> newGraph = new ArrayList<>();
 
-        for (GraphNode current : graph) {
-            if (!explored.contains(current)) {
-                toExplore.offer(current);
-                while (!toExplore.isEmpty()) {
-                    GraphNode toExploreNode = toExplore.poll();
-                    GraphNode copyNode = new GraphNode(toExploreNode.key);
-                    nodeMapping.put(toExploreNode, copyNode);
-                    explored.add(toExploreNode);
+        for (GraphNode currentNode : graph) {
+            if (!expanded.contains(currentNode)) {
+                toExpandNodes.offer(currentNode);
+                GraphNode currentNodeCopy = new GraphNode(currentNode.key);
+                nodeMapping.put(currentNode, currentNodeCopy);
+                while (toExpandNodes.size() != 0) {
+                    GraphNode toExpandNode = toExpandNodes.poll();
+                    if (expanded.contains(toExpandNode)) {
+                        continue;
+                    }
 
-                    for (GraphNode neighbor : toExploreNode.neighbors) {
-                        if (!explored.contains(neighbor)) {
-                            toExplore.offer(neighbor);
-                            GraphNode copyNeighborNode = new GraphNode(neighbor.key);
-                            nodeMapping.put(neighbor, copyNeighborNode);
+                    GraphNode toExpandNodeCopy = new GraphNode(toExpandNode.key);
+                    nodeMapping.put(toExpandNode, toExpandNodeCopy);
+                    expanded.add(toExpandNode);
+                    toExpandNodes.offer(toExpandNode);
+                    for (GraphNode neighbor : toExpandNode.neighbors) {
+                        if (!nodeMapping.containsKey(neighbor)) {
+                            GraphNode neighborCopy = new GraphNode(neighbor.key);
+                            nodeMapping.put(neighbor, neighborCopy);
                         }
-                        copyNode.neighbors.add(nodeMapping.get(neighbor));
+                        toExpandNodes.offer(neighbor);
+                        toExpandNodeCopy.neighbors.add(nodeMapping.get(neighbor));
                     }
                 }
+                copiedGraph.add(nodeMapping.get(currentNode));
             }
-            newGraph.add(nodeMapping.get(current));
         }
-        return newGraph;
+        return copiedGraph;
     }
+
+
+    //DFS 的写法
+    public List<GraphNode> copy1(List<GraphNode> graph) {
+        Map<GraphNode, GraphNode> nodeMapping = new HashMap<>();
+        List<GraphNode> copyGraph = new ArrayList<>();
+        for (GraphNode currentNode: graph) {
+            copyGraph.add(cloneGraph(currentNode, nodeMapping));
+        }
+        return copyGraph;
+    }
+
+    private GraphNode cloneGraph(GraphNode current, Map<GraphNode, GraphNode> nodeMapping) {
+        if (current == null) {
+            return null;
+        }
+        if (nodeMapping.containsKey(current)) {
+            return nodeMapping.get(current);
+        }
+
+        GraphNode copiedNode = new GraphNode(current.key);
+        nodeMapping.put(current, copiedNode);
+        for (GraphNode neighbor : current.neighbors) {
+            copiedNode.neighbors.add(cloneGraph(neighbor, nodeMapping));
+        }
+        return copiedNode;
+    }
+
     public  class GraphNode {
     public int key;
     public List<GraphNode> neighbors;
