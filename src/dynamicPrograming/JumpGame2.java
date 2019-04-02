8d8e138c42a5698daee77ca205df1aab96a01afb
jiangchen <zjiangch@gmail.com>
2019-04-02 00:04:34 -0700

    update greedy for jump game2

--- src/dynamicPrograming/JumpGame2.java
+++ src/dynamicPrograming/JumpGame2.java
@@ -18,7 +18,49 @@ public class JumpGame2 {
         }
         return jumpStep[0] == Integer.MAX_VALUE ? -1 : jumpStep[0];
     }
+
+    //greedy 写法1 用remainder的思想
+    //每多条一步，看当前最远范围内的所有点，最多能留多少remainder给下一层
+    //结果的保存，类似于dp，填满一个一维的result数组，比如 位置2-> 1   3-> 2步
+    //用滚动数组的方式优化到其实只用一个int来记录当前最小的步数）
+    //again，不是一拍脑门想出来的 orz
+    public int jumpGreedy1(int[] nums) {
+        int currentJumps = 0;
+        int currentIndex = 1;
+        int nextRemain = nums[0];
+        int currentRemain = 0;
+        while (currentIndex < nums.length) {
+            currentJumps++;
+            currentRemain = nextRemain;
+            nextRemain = 0;
+            while (currentRemain > 0 && currentIndex < nums.length) {
+                currentRemain--;
+                nextRemain--;
+                nextRemain = Math.max(nextRemain, nums[currentIndex]);
+                currentIndex++;
+            }
+        }
+        return currentJumps;
+    }
+
+    //最优的greedy 答案是把remainder给简化掉了。而是保存当前jump能跳最远在哪，
+    // 然后liner scan当前范围内的点最多能支持我下一步跳到哪
+    public int jumpGreedy2(int[] nums) {
+        int currentJumps = 0;
+        int currentMax = 0;
+        int nextMax = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > currentMax) {
+                currentJumps++;
+                currentMax = nextMax;
+            }
+            nextMax = Math.max(nextMax, nums[i] + i);
+
+        }
+        return currentJumps;
+    }
 }
+
 
 
 /*
