e63d59f825cf275d1cfbbd8911bf4019e0145014
jiangchen <zjiangch@gmail.com>
2019-04-04 19:37:05 -0700

    fix bug

--- src/dynamicPrograming/LargestSquareOf1s.java
+++ src/dynamicPrograming/LargestSquareOf1s.java
@@ -46,8 +46,8 @@ public class LargestSquareOf1s {
         int[][] dp = new int[m][n];
         int max = 0;
 
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (i == 0 || j == 0) {
                     dp[i][j] = matrix[i][j] - '0';
                 } else if (matrix[i][j] == '1') {
